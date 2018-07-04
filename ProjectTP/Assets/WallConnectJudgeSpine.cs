1aee5164d62e5db57f6324dd6ef1952949966bd9
crazymarks <crazymarks@live.com>
2018-07-04 15:11:07 +0900

    いろいろ修正　ステージ３組み立て 逆転した針を追加

--- ProjectTP/Assets/WallConnectJudgeSpine.cs
+++ ProjectTP/Assets/WallConnectJudgeSpine.cs
@@ -2,29 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallConnectJudge : MonoBehaviour {
-    private int terrainCount=0;//地形の数を計数
-    private int timeCount=0; // 時間を計算
-    private SpriteRenderer SR;
+public class WallConnectJudgeSpine : MonoBehaviour {
+    private int terrainCount = 0;//地形の数を計数
+    private int timeCount = 0; // 時間を計算
+    private MeshRenderer MR;
 
-    void Start()
+    void Update()
     {
-            SR = this.transform.parent.gameObject.GetComponent<SpriteRenderer>();
-    }
-    void Update () {
         if (terrainCount == 0)
         {
             timeCount++;
             if (timeCount >= 5)//５フレーム後、地形と接触しない場合は消える
             {
-                SR.material.color = new Vector4(SR.material.color.r, SR.material.color.g, SR.material.color.b, SR.material.color.a - 0.01f);
-                if (SR.material.color.a < 0)
+                MR = this.transform.parent.gameObject.GetComponent<MeshRenderer>();
+                MR.material.color = new Vector4(MR.material.color.r, MR.material.color.g, MR.material.color.b, MR.material.color.a - 0.01f);
+                if (MR.material.color.a < 0)
                 {
                     Destroy(this.transform.parent.gameObject);
                 }
             }
         }
-	}
+    }
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Terrain")
